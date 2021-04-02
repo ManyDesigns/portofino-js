4ea2d3d6ff05cae2247281ce67da4d868650c398
Mattia Dapino <mattia.dapino@hey.com>
2021-04-02 09:02:06 +0200

    updated yarn

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,7 +4,7 @@
  * If the separator occurs at the concatenation boundary in either of the strings, it is removed to prevent duplication of the separator.
  *
  * @param {String} part1 First string.
- * @param {String} part2 Second string.
+ * @param {String|number} part2 Second string.
  *
  * @returns {String} Joined string.
  *
@@ -26,7 +26,7 @@ export function joinPath(part1: string, part2?: string | number): string {
 
   let p2 = part2.toString();
   p2 = p2.startsWith(separator) ? p2.slice(separator.length) : p2;
-  
+
   return p1 === '' || p2 === ''
     ? p1 + p2
     : p1 + separator + p2;
