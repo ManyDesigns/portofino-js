92dd4091a07b546907ee1f39c6b5cb3e3f645894
Mattia Dapino <mattia.dapino@hey.com>
2021-05-14 16:12:00 +0200

    Replaced vitepress with vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  title: 'Portofino JS',
+  description: 'Portofino javascript API',
+  lang: 'it-IT',
+  base: '/portofino-js/',
+
+  themeConfig: {
+    nav: [
+      { text: 'Guide', link: '/', activeMatch: '^/$|^/guide/' },
+      {
+        text: 'Changelogs',
+        link: 'https://github.com/manydesigns/portofino-js/releases'
+      }, {
+        text: 'GitHub',
+        link: 'https://github.com/manydesigns/portofino-js'
+      },
+    ],
+
+    sidebar: {
+      '/': [
+        {
+          title: 'Introduzione',
+          children: [
+            { title: 'Perchè usare PortofinoJS?', path: '/' },
+            { title: 'Getting Started', path: '/getting-started' },
+            { title: 'Configurazione', path: '/config' },
+          ]
+        },
+        {
+          title: 'Actions',
+          children: [
+            { title: 'Ottenere una action', path: '/actions/' },
+            {
+              title: 'Crud Actions',
+              path: '/actions/crud-action/',
+              children: [
+                { title: 'Operazioni CRUD', path: '/actions/crud-action/crud' },
+                // { title: 'Entità CRUD', path: '/actions/crud-action/entities' },
+                // { title: 'Metodi di utilità', path: '/actions/crud-action/crud-utilities' },
+              ]
+            },
+            { title: 'Login Actions', path: '/actions/login-action' },
+          ]
+        },
+      ]
+    }
+  }
+}
