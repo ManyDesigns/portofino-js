d5bf5cd78893eca1378742b78163d520b27acfab
Mattia Dapino <mattia.dapino@hey.com>
2021-07-16 09:06:33 +0200

    fix tsconfig

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,14 @@ import path from 'path';
 import typescript from '@rollup/plugin-typescript';
 
 module.exports = {
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src')
-      }
-    ]
-  },
   build: {
     sourcemap: true,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'Portofino'
+      name: 'Portofino',
     },
     rollupOptions: {
-      plugins: [typescript()]
-    }
-  }
+      plugins: [typescript()],
+    },
+  },
 };
