3ebc93e687ea656f7a4421fb5894f7068d259123
Mattia Dapino <mattia.dapino@hey.com>
2021-08-10 22:48:30 +0200

    feat: Selection provider types

--- src/actions/crudAction/SelectionProvider.ts
+++ src/actions/crudAction/SelectionProvider.ts
@@ -1,16 +1,48 @@
-import NooNoo from "../../NooNoo";
+import NooNoo from '../../NooNoo';
+import {
+  SelectMode,
+  SearchSelectMode,
+  SelectionProviderOption,
+} from '../../types/SelectionProviderTypes';
 
 export default class SelectionProvider {
-  constructor(
-    private http: NooNoo,
-    public searchDisplayMode: string,
-    public fieldNames: string[],
-    public name: string,
-    public displayMode: string
-  ) {}
+  readonly #http: NooNoo;
 
-  async getOptions() {
-    const { data } = await this.http.get(`:selectionProvider/${this.name}`);
+  public name: string;
+  public fieldNames: string[];
+  public selectMode: SelectMode;
+  public searchSelectMode: SearchSelectMode;
+
+  constructor(
+    http: NooNoo,
+    name: string,
+    fieldNames: string[],
+    selectMode: SelectMode,
+    searchSelectMode: SearchSelectMode
+  ) {
+    this.#http = http;
+    this.name = name;
+    this.fieldNames = fieldNames;
+    this.selectMode = selectMode;
+    this.searchSelectMode = searchSelectMode;
+  }
+
+  /**
+   * @deprecated use selectMode
+   */
+  get displayMode() {
+    return this.selectMode;
+  }
+
+  /**
+   * @deprecated use searchSelectMode
+   */
+  get searchDisplayMode() {
+    return this.searchSelectMode;
+  }
+
+  async getOptions(): Promise<SelectionProviderOption[]> {
+    const { data } = await this.#http.get(`:selectionProvider/${this.name}`);
     return data;
   }
 }
